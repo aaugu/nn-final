46fb24a22130c400f24627a86d3358282692bceb
Anais Augu <augu.anais@gmail.com>
2022-08-19 15:04:49 +0200

    News page
    Route, controller method and view of news page created. Add possibility to open a pdf file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	root to: 'pages#home'
 	get 'about', to: 'pages#about'
 	get 'contact', to: 'pages#contact'
+  get 'news', to: 'pages#news'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
