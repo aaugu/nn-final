fda1a1ccc03025ff9cb5d0867448826e8150ca2d
Anais Augu <augu.anais@gmail.com>
2022-08-23 19:30:30 +0200

    psy-cards height position fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, :content, :url, presence: true
-  validates :url, format: {with: /\Ahttps:\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/}
+  validates :url, format: {with: /\https:\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/}
 end
